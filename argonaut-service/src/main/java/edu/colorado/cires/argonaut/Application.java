b438915c9283acbce4118c327f3b872a8bbd1eb1
Chris Slater <6004777+electricsam@users.noreply.github.com>
2025-05-02 08:52:19 -0600

    add prod resources

--- argonaut-service/src/main/java/edu/colorado/cires/argonaut/Application.java
+++ argonaut-service/src/main/java/edu/colorado/cires/argonaut/Application.java
@@ -8,12 +8,14 @@ import org.springframework.boot.system.ApplicationHome;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args){
+  static {
     System.setProperty("camel.threads.virtual.enabled", "true");
-
     File svcHome = new ApplicationHome().getDir();
     String path = svcHome.getAbsolutePath();
     System.setProperty("svc.home", path);
+  }
+
+  public static void main(String[] args){
     SpringApplication.run(Application.class, args);
   }
 }
