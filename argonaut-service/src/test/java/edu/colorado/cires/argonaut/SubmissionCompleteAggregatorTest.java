8a628fe8e9e07658c5ee691ebfb5b4fc2fdc7e58
Chris Slater <6004777+electricsam@users.noreply.github.com>
2025-05-07 15:04:52 -0600

    add file removal email agg test

--- argonaut-service/src/test/java/edu/colorado/cires/argonaut/SubmissionCompleteAggregatorTest.java
+++ argonaut-service/src/test/java/edu/colorado/cires/argonaut/SubmissionCompleteAggregatorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.colorado.cires.argonaut.config.ServiceProperties;
 import edu.colorado.cires.argonaut.message.NcSubmissionMessage;
+import edu.colorado.cires.argonaut.message.NcSubmissionMessage.Operation;
 import edu.colorado.cires.argonaut.message.SubmissionCompleteMessage;
 import edu.colorado.cires.argonaut.route.QueueConsts;
 import java.time.Instant;
@@ -45,7 +46,7 @@ public class SubmissionCompleteAggregatorTest {
   private ProducerTemplate producerTemplate;
 
   @Test
-  public void testHappyPath() throws Exception {
+  public void testDataSubmission() throws Exception {
 
     Instant timestamp = Instant.now();
 
@@ -91,6 +92,102 @@ public class SubmissionCompleteAggregatorTest {
         .collect(Collectors.toSet());
 
     assertEquals(expected, actual);
+
+  }
+
+
+  @Test
+  public void testRemoval() throws Exception {
+
+    String timestamp = Instant.now().toString();
+    String dac = "aoml";
+
+    SubmissionCompleteMessage expectedMessage = new SubmissionCompleteMessage();
+    expectedMessage.setTimeStamp(timestamp);
+    expectedMessage.setNumberOfFiles(13);
+    expectedMessage.setDac(dac);
+
+    Set<NcSubmissionMessage> messages = new HashSet<>();
+
+    for (String fileName : Arrays.asList(
+        "R5904398_001.nc",
+        "R5904398_004.nc",
+        "R5904398_005.nc",
+        "R5904398_006.nc",
+        "5904398_Rtraj.nc",
+        "5904398_meta.nc",
+        "R5904397_001.nc",
+        "R5904397_004.nc",
+        "R5904397_005.nc",
+        "R5904397_006.nc",
+        "5904397_Rtraj.nc",
+        "5904397_meta.nc",
+        "5904397_tech.nc")
+    ) {
+
+      String floatId = fileName.replaceAll("R", "").split("_")[0];
+
+      NcSubmissionMessage message = new NcSubmissionMessage();
+      message.setFloatId(floatId);
+      message.setDac(dac);
+      message.setTimestamp(timestamp);
+      message.setFileName(fileName);
+      message.setProfile(fileName.contains("R"));
+      message.setNumberOfFilesInSubmission(13);
+      message.setOperation(Operation.REMOVE);
+
+      messages.add(message);
+
+      expectedMessage.getFilesCompleted().add(fileName);
+
+    }
+
+    prepareEmail.expectedMessageCount(1);
+
+    messages.forEach(message -> producerTemplate.sendBody(QueueConsts.SUBMISSION_COMPLETE_AGG, message));
+
+    prepareEmail.assertIsSatisfied();
+
+    SubmissionCompleteMessage actual = prepareEmail.getExchanges().stream()
+        .map(exchange -> exchange.getIn().getBody(SubmissionCompleteMessage.class))
+        .findFirst().orElse(null);
+
+    assertEquals(expectedMessage, actual);
+
+  }
+
+
+  @Test
+  public void testInvalidRemoval() throws Exception {
+
+    String timestamp = Instant.now().toString();
+    String dac = "aoml";
+
+    SubmissionCompleteMessage expectedMessage = new SubmissionCompleteMessage();
+    expectedMessage.setTimeStamp(timestamp);
+    expectedMessage.setNumberOfFiles(1);
+    expectedMessage.setDac(dac);
+    expectedMessage.getFilesCompleted().add("foobar_removal.txt");
+
+    NcSubmissionMessage message = new NcSubmissionMessage();
+    message.setDac(dac);
+    message.setTimestamp(timestamp);
+    message.setFileName("foobar_removal.txt");
+    message.setProfile(false);
+    message.setNumberOfFilesInSubmission(1);
+    message.setOperation(Operation.REMOVE);
+
+    prepareEmail.expectedMessageCount(1);
+
+    producerTemplate.sendBody(QueueConsts.SUBMISSION_COMPLETE_AGG, message);
+
+    prepareEmail.assertIsSatisfied();
+
+    SubmissionCompleteMessage actual = prepareEmail.getExchanges().stream()
+        .map(exchange -> exchange.getIn().getBody(SubmissionCompleteMessage.class))
+        .findFirst().orElse(null);
+
+    assertEquals(expectedMessage, actual);
 
   }
 
